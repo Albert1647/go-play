8aebbd4786d3d28e6b4ed308d3db7345ff30cb56
Natthadecha Kijsamrej <pittaya.sc2@gmail.com>
2024-03-07 13:47:04 +0700

    137

--- app.go
+++ app.go
@@ -1,19 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"natthan.com/go-play/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxInclidePrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxInclidePrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxInclidePrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,14 +1,25 @@
 package prices
 
+import "fmt"
+
 type TaxIncludePriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
+func (job TaxIncludePriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+	fmt.Println(result)
+}
+
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludePriceJob {
 	return &TaxIncludePriceJob{
-		InputPrices: []float64{0, 0.07, 0.1, 0.15},
+		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
 }
