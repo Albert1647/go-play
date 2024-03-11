8059df4239750bd05114a7fd14d032f9f22fb373
Natthadecha Kijsamrej <pittaya.sc2@gmail.com>
2024-03-11 13:42:41 +0700

    Update app.go

--- app.go
+++ app.go
@@ -2,10 +2,48 @@ package main
 
 import "fmt"
 
-func main() {
-	output("Hello World!")
+type TaxRate struct {
+	base float64
+	rate float64
 }
 
-func output(array ...interface{}) {
-	fmt.Println(array...)
+func main() {
+	taxLadder := []TaxRate{
+		{
+			base: 150_000,
+			rate: 0.05,
+		},
+		{
+			base: 300_000,
+			rate: 0.10,
+		},
+		{
+			base: 500_000,
+			rate: 0.15,
+		},
+		{
+			base: 1_000_000,
+			rate: 0.20,
+		},
+		{
+			base: 2_000_000,
+			rate: 0.25,
+		},
+		{
+			base: 5_000_000,
+			rate: 0.30,
+		},
+	}
+	calculateTaxSalary(&taxLadder)
+}
+
+func calculateTaxSalary(tax *[]TaxRate) {
+	for _, tax := range *tax {
+		var taxSalary = tax.base / 12
+		var taxLostPerSalary = taxSalary * tax.rate
+		var taxLeft = taxSalary - taxLostPerSalary
+		fmt.Printf("Base = %.2f Rate %.2f = Salary %.2f, Lost %.2f,  Left %.2f\n",
+			tax.base, tax.rate, taxSalary, taxLostPerSalary, taxLeft,
+		)
+	}
 }
