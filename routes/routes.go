501642054998044eb64d7796f64f561d14bb5d33
Natthadecha Kijsamrej <pittaya.sc2@gmail.com>
2024-03-27 05:47:09 +0700

    💭try group version

--- routes/routes.go
+++ routes/routes.go
@@ -6,24 +6,40 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
-	server.GET("/health", getHealth)
-	server.GET("/users", getUsers)
-	server.GET("/users/:id", getUserByID)
+	v1 := server.Group("/v1")
+	{
+		v1.GET("/health", getHealth)
+		v1.GET("/users", getUsers)
+		v1.GET("/users/:id", getUserByID)
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEventByID)
+		v1.GET("/events", getEvents)
+		v1.GET("/events/:id", getEventByID)
 
-	server.GET("/registration", getRegistrations)
+		v1.GET("/registration", getRegistrations)
 
-	authenticated := server.Group("/")
-	authenticated.Use(middlewares.Authenticate)
-	authenticated.POST("/events", createEvent)
-	authenticated.PUT("/events/:id", updateEvent)
-	authenticated.DELETE("/events/:id", deleteEvent)
+		v1.POST("/signup", signup)
+		v1.POST("/login", login)
+	}
+	authenticated := v1.Group("").Use(middlewares.Authenticate)
+	{
+		authenticated.POST("/events", createEvent)
+		authenticated.PUT("/events/:id", updateEvent)
+		authenticated.DELETE("/events/:id", deleteEvent)
+		authenticated.POST("/events/:id/register", registerForEvent)
+		authenticated.DELETE("/events/:id/register", cancelRegistration)
+	}
+	v2 := server.Group("/v2")
+	{
+		v2.GET("/health", getHealth)
+		v2.GET("/users", getUsers)
+		v2.GET("/users/:id", getUserByID)
 
-	authenticated.POST("/events/:id/register", registerForEvent)
-	authenticated.DELETE("/events/:id/register", cancelRegistration)
+		v2.GET("/events", getEvents)
+		v2.GET("/events/:id", getEventByID)
 
-	server.POST("/signup", signup)
-	server.POST("/login", login)
+		v2.GET("/registration", getRegistrations)
+
+		v2.POST("/signup", signup)
+		v2.POST("/login", login)
+	}
 }
