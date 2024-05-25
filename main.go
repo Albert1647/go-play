cebf8c62d14a968682bfe404532efbdc3d7d3b91
Natthadecha Kijsamrej <pittaya.sc2@gmail.com>
2024-05-25 16:51:10 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
 }
 
@@ -13,14 +13,21 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-
 	done := make(chan bool)
+	// 	dones := make([]chan bool, 4)
+	// dones[0] = make(chan bool)
+	// 	dones[1] = make(chan bool)
+	// 	dones[2] = make(chan bool)
+	// 	dones[3] = make(chan bool)
+	go greet("Nice to Meet You!", done)
+	go greet("How are you!", done)
 	go slowGreet("How.. Are.. You..", done)
-	<-done
-	// 	go greet("Nice to Meet You!")
-	// 	go greet("How are you!")
-	// 	go greet("Hope you have a great day")
+	go greet("Hope you have a great day", done)
+	for range done {
+		// fmt.Println(doneChan)
+	}
 }
